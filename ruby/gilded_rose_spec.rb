fe12001e61ff86784eb8b8c1fa47f6b7a288aa22
andrii <avoinarovskiy@gmail.com>
2024-05-27 16:23:17 +0300

    refactor specs

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -3,161 +3,106 @@ require 'rspec'
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  def create_item(name, sell_in, quality)
+  def create_item(name, sell_in = 1, quality = 2)
     Item.new(name, sell_in, quality)
   end
 
+  let(:name) { "foo" }
+  let(:sell_in) { 5 }
+  let(:quality) { 5 }
+
   before(:each) do
-    GildedRose.new(items).update_quality()
+    @items = [create_item(name, sell_in, quality)]
+    GildedRose.new(@items).update_quality()
+  end
+
+  shared_examples "a normal item" do |quality_changes|
+    it "does not change the name" do
+      expect(@items[0].name).to eq name
+    end
+
+    it "decreases sell_in by 1" do
+      expect(@items[0].sell_in).to eq sell_in - 1
+    end
+
+    it "changes quality by #{quality_changes}" do
+      expect(@items[0].quality).to eq quality + quality_changes
+    end
   end
 
   context "foo" do
     context "not last day" do
-      let(:items) { [create_item("foo", 1, 2)] }
-
-      it "does not change the name" do
-        expect(items[0].name).to eq "foo"
-      end
-
-      it "decreases sell_in by 1" do
-        expect(items[0].sell_in).to eq 0
-      end
-  
-      it "decreases quality by 1" do
-        expect(items[0].quality).to eq 1
-      end
+      it_behaves_like "a normal item", -1
     end
 
     context "last day" do
-      let(:items) { [create_item("foo", 0, 5)] }
+      let(:sell_in) { 0 }
+      it_behaves_like "a normal item", -2
+    end
+  end
+
+  context "Aged Brie" do
+    let(:name) { "Aged Brie" }
+
+    context "before sell_in period" do 
+      it_behaves_like "a normal item", 1
+    end
+
+    context "after sell_in period" do 
+      let(:sell_in) { 0 }
+      it_behaves_like "a normal item", 2
+    end
+  end
+
+  context "Backstage passes to a TAFKAL80ETC concert" do
+    let(:name) { "Backstage passes to a TAFKAL80ETC concert" }
+
+    context "more than 10 days before concert" do
+      let(:sell_in) { 15 }
+      it_behaves_like "a normal item", 1
+    end
+
+    context "more than 5 days before concert" do
+      let(:sell_in) { 10 }
+      it_behaves_like "a normal item", 2
+    end
+
+    context "less than 5 days before concert" do
+      let(:sell_in) { 5 }
+      it_behaves_like "a normal item", 3
+    end
+
+    context "after concert" do
+      let(:sell_in) { 0 }
 
       it "does not change the name" do
-        expect(items[0].name).to eq "foo"
-      end
-
-      it "decreases sell_in by 1" do
-        expect(items[0].sell_in).to eq -1
+        expect(@items[0].name).to eq name
       end
   
-      it "decreases quality by 2" do
-        expect(items[0].quality).to eq 3
+      it "decreases sell_in by 1" do
+        expect(@items[0].sell_in).to eq sell_in - 1
+      end
+
+      it "sets quality to 0" do
+        expect(@items[0].quality).to eq 0
       end
     end
   end
 
   context "Sulfuras, Hand of Ragnaros" do
-    let(:items) { [Item.new("Sulfuras, Hand of Ragnaros", 5, 80)] }
+    let(:name) { "Sulfuras, Hand of Ragnaros" }
+    let(:quality) { 80 }
 
     it "does not change the name" do
-      expect(items[0].name).to eq "Sulfuras, Hand of Ragnaros"
+      expect(@items[0].name).to eq "Sulfuras, Hand of Ragnaros"
     end
 
     it "does not change the sell_in" do
-      expect(items[0].sell_in).to eq 5
+      expect(@items[0].sell_in).to eq sell_in
     end
   
     it "does not change the quality" do
-      expect(items[0].quality).to eq 80
-    end
-  end
-
-  context "Aged Brie" do
-    context "before sell_in period" do 
-      let(:items) { [Item.new("Aged Brie", 5, 0)] }
-
-      it "does not change the name" do
-        expect(items[0].name).to eq "Aged Brie"
-      end
-  
-      it "decreases sell_in by 1" do
-        expect(items[0].sell_in).to eq 4
-      end
-    
-      it "increases quality by 1" do
-        expect(items[0].quality).to eq 1
-      end
-    end
-
-    context "after sell_in period" do 
-      let(:items) { [Item.new("Aged Brie", 0, 0)] }
-
-      it "does not change the name" do
-        expect(items[0].name).to eq "Aged Brie"
-      end
-  
-      it "decreases sell_in by 1" do
-        expect(items[0].sell_in).to eq -1
-      end
-    
-      it "increases quality by 2" do
-        expect(items[0].quality).to eq 2
-      end
-    end
-  end
-
-  context "Backstage passes to a TAFKAL80ETC concert" do
-    context "more than 10 days before" do
-      let(:items) { [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 10)] }
-      
-      it "does not change the name" do
-        expect(items[0].name).to eq "Backstage passes to a TAFKAL80ETC concert"
-      end
-
-      it "decreases sell_in by 1" do
-        expect(items[0].sell_in).to eq 14
-      end
-    
-      it "increases quality by 1" do
-        expect(items[0].quality).to eq 11
-      end
-    end
-
-    context "more than 5 days before" do
-      let(:items) { [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)] }
-
-      it "does not change the name" do
-        expect(items[0].name).to eq "Backstage passes to a TAFKAL80ETC concert"
-      end
-
-      it "decreases sell_in by 1" do
-        expect(items[0].sell_in).to eq 9
-      end
-    
-      it "increases quality by 2" do
-        expect(items[0].quality).to eq 12
-      end
-    end
-
-    context "less than 5 days before" do
-      let(:items) { [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)] }
-
-      it "does not change the name" do
-        expect(items[0].name).to eq "Backstage passes to a TAFKAL80ETC concert"
-      end
-
-      it "decreases sell_in by 1" do
-        expect(items[0].sell_in).to eq 4
-      end
-    
-      it "increases quality by 3" do
-        expect(items[0].quality).to eq 13
-      end
-    end
-
-    context "after concert" do
-      let(:items) { [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)] }
-
-      it "does not change the name" do
-        expect(items[0].name).to eq "Backstage passes to a TAFKAL80ETC concert"
-      end
-
-      it "decreases sell_in by 1" do
-        expect(items[0].sell_in).to eq -1
-      end
-    
-      it "sets quality to 0" do
-        expect(items[0].quality).to eq 0
-      end
+      expect(@items[0].quality).to eq quality
     end
   end
 end
