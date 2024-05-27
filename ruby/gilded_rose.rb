83873e9d4a29da8c794b6024656f861ca1dae34e
andrii <avoinarovskiy@gmail.com>
2024-05-27 18:31:09 +0300

    refactor with rubocop

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,14 +1,13 @@
 class GildedRose
-
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      next if item.name == "Sulfuras, Hand of Ragnaros"
+      next if item.name == 'Sulfuras, Hand of Ragnaros'
 
-      if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert"
+      if item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert'
         increase_quality item
       else
         decrease_quality item
@@ -22,8 +21,8 @@ class GildedRose
   private
 
   def update_quality_for_overdue(item)
-    if item.name != "Aged Brie"
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+    if item.name != 'Aged Brie'
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
         item.quality = 0
       else
         decrease_quality item
@@ -34,19 +33,19 @@ class GildedRose
   end
 
   def increase_quality(item)
-    if item.quality < 50
-      item.quality += 1
+    return unless item.quality < 50
 
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        item.quality += 1 if item.sell_in < 11 and item.quality < 50
-        item.quality += 1 if item.sell_in < 6 and item.quality < 50
-      end
-    end
+    item.quality += 1
+
+    return unless item.name == 'Backstage passes to a TAFKAL80ETC concert'
+
+    item.quality += 1 if item.sell_in < 11 && item.quality < 50
+    item.quality += 1 if item.sell_in < 6 && item.quality < 50
   end
 
   def decrease_quality(item)
     item.quality -= 1 if item.quality > 0
-    item.quality -= 1 if item.quality > 0 and item.name == "Conjured Mana Cake"
+    item.quality -= 1 if item.quality > 0 && item.name == 'Conjured Mana Cake'
   end
 
   def decrease_sell_in(item)
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -7,21 +7,21 @@ describe GildedRose do
     Item.new(name, sell_in, quality)
   end
 
-  let(:name) { "foo" }
+  let(:name) { 'foo' }
   let(:sell_in) { 5 }
   let(:quality) { 5 }
 
   before(:each) do
     @items = [create_item(name, sell_in, quality)]
-    GildedRose.new(@items).update_quality()
+    GildedRose.new(@items).update_quality
   end
 
-  shared_examples "a normal item" do |quality_changes|
-    it "does not change the name" do
+  shared_examples 'a normal item' do |quality_changes|
+    it 'does not change the name' do
       expect(@items[0].name).to eq name
     end
 
-    it "decreases sell_in by 1" do
+    it 'decreases sell_in by 1' do
       expect(@items[0].sell_in).to eq sell_in - 1
     end
 
@@ -30,129 +30,129 @@ describe GildedRose do
     end
   end
 
-  context "foo" do
-    context "not overdue" do
-      include_examples "a normal item", -1
+  context 'foo' do
+    context 'not overdue' do
+      include_examples 'a normal item', -1
     end
 
-    context "overdue" do
+    context 'overdue' do
       let(:sell_in) { 0 }
-      include_examples "a normal item", -2
+      include_examples 'a normal item', -2
     end
 
-    context "with zero quality" do
+    context 'with zero quality' do
       let(:quality) { 0 }
-      include_examples "a normal item", 0
-      context "and overdue" do
+      include_examples 'a normal item', 0
+      context 'and overdue' do
         let(:sell_in) { 0 }
-        include_examples "a normal item", 0
+        include_examples 'a normal item', 0
       end
     end
   end
 
-  context "Conjured Mana Cake" do
-    let(:name) { "Conjured Mana Cake" }
-    context "not overdue" do
-      include_examples "a normal item", -2
-    end
-    
-    context "overdue" do
-      let(:sell_in) { 0 }
-      include_examples "a normal item", -4
+  context 'Conjured Mana Cake' do
+    let(:name) { 'Conjured Mana Cake' }
+    context 'not overdue' do
+      include_examples 'a normal item', -2
     end
 
-    context "with zero quality" do
+    context 'overdue' do
+      let(:sell_in) { 0 }
+      include_examples 'a normal item', -4
+    end
+
+    context 'with zero quality' do
       let(:quality) { 0 }
-      include_examples "a normal item", 0
-      context "and overdue" do
+      include_examples 'a normal item', 0
+      context 'and overdue' do
         let(:sell_in) { 0 }
-        include_examples "a normal item", 0
+        include_examples 'a normal item', 0
       end
     end
 
-    context "with quality = 1 " do
+    context 'with quality = 1 ' do
       let(:quality) { 1 }
-      include_examples "a normal item", -1
-      context "and overdue" do
+      include_examples 'a normal item', -1
+      context 'and overdue' do
         let(:sell_in) { 0 }
-        include_examples "a normal item", -1
+        include_examples 'a normal item', -1
       end
     end
   end
 
-  context "Aged Brie" do
-    let(:name) { "Aged Brie" }
+  context 'Aged Brie' do
+    let(:name) { 'Aged Brie' }
 
-    context "before sell_in period" do 
-      include_examples "a normal item", 1
+    context 'before sell_in period' do
+      include_examples 'a normal item', 1
     end
 
-    context "after sell_in period" do 
+    context 'after sell_in period' do
       let(:sell_in) { 0 }
-      include_examples "a normal item", 2
+      include_examples 'a normal item', 2
     end
 
-    context "with high quality" do
+    context 'with high quality' do
       let(:sell_in) { 5 }
       let(:quality) { 50 }
-      include_examples "a normal item", 0
+      include_examples 'a normal item', 0
     end
   end
 
-  context "Backstage passes to a TAFKAL80ETC concert" do
-    let(:name) { "Backstage passes to a TAFKAL80ETC concert" }
+  context 'Backstage passes to a TAFKAL80ETC concert' do
+    let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
 
-    context "more than 10 days before concert" do
+    context 'more than 10 days before concert' do
       let(:sell_in) { 15 }
-      include_examples "a normal item", 1
+      include_examples 'a normal item', 1
     end
 
-    context "more than 5 days before concert" do
+    context 'more than 5 days before concert' do
       let(:sell_in) { 10 }
-      include_examples "a normal item", 2
+      include_examples 'a normal item', 2
     end
 
-    context "less than 5 days before concert" do
+    context 'less than 5 days before concert' do
       let(:sell_in) { 5 }
-      include_examples "a normal item", 3
+      include_examples 'a normal item', 3
     end
 
-    context "after concert" do
+    context 'after concert' do
       let(:sell_in) { 0 }
 
-      it "does not change the name" do
+      it 'does not change the name' do
         expect(@items[0].name).to eq name
       end
-  
-      it "decreases sell_in by 1" do
+
+      it 'decreases sell_in by 1' do
         expect(@items[0].sell_in).to eq sell_in - 1
       end
 
-      it "sets quality to 0" do
+      it 'sets quality to 0' do
         expect(@items[0].quality).to eq 0
       end
     end
 
-    context "with high quality" do
+    context 'with high quality' do
       let(:sell_in) { 5 }
       let(:quality) { 49 }
-      include_examples "a normal item", 1
+      include_examples 'a normal item', 1
     end
   end
 
-  context "Sulfuras, Hand of Ragnaros" do
-    let(:name) { "Sulfuras, Hand of Ragnaros" }
+  context 'Sulfuras, Hand of Ragnaros' do
+    let(:name) { 'Sulfuras, Hand of Ragnaros' }
     let(:quality) { 80 }
 
-    it "does not change the name" do
-      expect(@items[0].name).to eq "Sulfuras, Hand of Ragnaros"
+    it 'does not change the name' do
+      expect(@items[0].name).to eq name
     end
 
-    it "does not change the sell_in" do
+    it 'does not change the sell_in' do
       expect(@items[0].sell_in).to eq sell_in
     end
-  
-    it "does not change the quality" do
+
+    it 'does not change the quality' do
       expect(@items[0].quality).to eq quality
     end
   end
